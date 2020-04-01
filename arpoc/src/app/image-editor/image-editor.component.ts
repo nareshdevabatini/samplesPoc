ddba9f75976a7c580423b9378241476a50bf1d2b
nareshdevabatini <naresh.devabatini@gmail.com>
2020-04-01 16:37:13 +0530

    Added default images

--- arpoc/src/app/image-editor/image-editor.component.ts
+++ arpoc/src/app/image-editor/image-editor.component.ts
@@ -1,15 +1,98 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { ToastUiImageEditorComponent } from 'ngx-tui-image-editor';
+import ImageEditor from "tui-image-editor";
 
 @Component({
   selector: 'app-image-editor',
   templateUrl: './image-editor.component.html',
 })
-export class ImageEditorComponent implements AfterViewInit {
+export class ImageEditorComponent implements AfterViewInit, OnInit {
   title = 'Image Editor';
   @ViewChild(ToastUiImageEditorComponent) editorComponent: ToastUiImageEditorComponent;
 
+  public ngOnInit() {
+    // this.editorComponent.options.includeUI = {
+    //   loadImage: {
+    //     path: '../../assests/bodyimage.jpg',
+    //     name: 'SampleImage',
+    //   }
+    // }
+    // this.editorComponent = {
+    //   includeUI: true,
+    //   options: {
+    //     includeUI: {
+    //       loadImage: {
+    //         path: 'img/sampleImage.jpg',
+    //         name: 'SampleImage',
+    //       }
+    //     },
+    //     cssMaxWidth: 700,
+    //     cssMaxHeight: 500,
+    //     selectionStyle: {
+    //       cornerSize: 20,
+    //       rotatingPointOffset: 70
+    //     }
+    //   },
+    //   addText: null,
+    //   mousedown: null,
+    //   objectActivated: null,
+    //   objectMoved: null,
+    //   objectScaled: null,
+    // }
+
+  }
   ngAfterViewInit() {
-    console.log(this.editorComponent.editorInstance);
+
+    this.editorComponent.editorInstance = new ImageEditor(
+      document.querySelector('#tui-image-editor'), {
+      cssMaxWidth: 500,
+      cssMaxHeight: 500,
+      selectionStyle: {
+        cornerSize: 20,
+        rotatingPointOffset: 70
+      },
+      includeUI: {
+        loadImage: {
+          path: '../../assets/cau_male_head_anterior.jpg',
+          name: 'Blank'
+        },
+        menuBarPosition: 'bottom'
+      },
+    });
+   
+  }
+  public loadImage(type: string) {
+    switch (type) {
+      case "male_head":
+        this.editorComponent.editorInstance.loadImageFromURL('../../assets/cau_male_head_anterior.jpg', 'bodyimage').then(t => {
+          console.log(t);
+        });
+        break;
+      case "back_full_male_body":
+        this.editorComponent.editorInstance.loadImageFromURL('../../assets/cau_male_fullbody_posterior.jpg', 'bodyimage').then(t => {
+          console.log(t);
+        });
+        break;
+      case "front_full_male_body":
+        this.editorComponent.editorInstance.loadImageFromURL('../../assets/bodyimage.jpg', 'bodyimage').then(t => {
+          console.log(t);
+        });
+        break;
+      case "woman_body":
+        this.editorComponent.editorInstance.loadImageFromURL('../../assets/womanNew.gif', 'bodyimage').then(t => {
+          console.log(t);
+        });
+        break;
+      default:
+        this.editorComponent.editorInstance.loadImageFromURL('../../assets/blank.JPG', 'bodyimage').then(t => {
+          console.log(t);
+        });
+
+    }
+
+  }
+
+  public saveImage() {
+
   }
 }
--- arpoc/src/app/sketchpad/ar-sketchpad.component.ts
+++ arpoc/src/app/sketchpad/ar-sketchpad.component.ts
@@ -1,12 +1,36 @@
-import { Component, ViewChild } from '@angular/core';
-import { ViewerComponent, AR_EXPORTS, PdfExportService, HtmlExportService, XlsxExportService } from '@grapecity/activereports-angular';
-
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { ToastUiImageEditorComponent } from 'ngx-tui-image-editor';
+import ImageEditor from "tui-image-editor";
 
 @Component({
-    selector: 'app-sketchpad',
-    templateUrl: './ar-sketchpad.component.html',
-  })
-  export class ARSketchPadComponent {
-    title = 'Sketchpad';
-   
+  selector: 'app-sketchpad',
+  templateUrl: './ar-sketchpad.component.html',
+  styleUrls: ['./ar-sketchpad.component.css']
+})
+export class ARSketchPadComponent implements AfterViewInit {
+  title = 'Sketchpad';
+  @ViewChild(ToastUiImageEditorComponent) editorComponent: ToastUiImageEditorComponent;
+  ngAfterViewInit() {
+
+    this.editorComponent.editorInstance = new ImageEditor(
+      document.querySelector('#tui-image-editor'), {
+      cssMaxWidth: 500,
+      cssMaxHeight: 500,
+      selectionStyle: {
+        cornerSize: 20,
+        rotatingPointOffset: 70
+      },
+      includeUI: {
+        loadImage: {
+          path: '../../assets/blank.JPG',
+          name: 'Blank'
+        },
+        menuBarPosition: 'bottom'
+      },
+    });
   }
+
+  public saveImage() {
+
+  }
+}
